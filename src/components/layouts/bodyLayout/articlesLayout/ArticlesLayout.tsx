fd0c1a462288f0c180bd84064763993ffc18a742
Noy Keren <noyk@moveohls.com>
2024-02-22 12:47:19 +0200

    body layout - articles layout and widgets layout

--- src/components/layouts/bodyLayout/articlesLayout/ArticlesLayout.tsx
+++ src/components/layouts/bodyLayout/articlesLayout/ArticlesLayout.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import { ArticleData } from "../../../card/articleCard/types";
-import { Stack } from "@mui/material";
+import { Paper, Stack, StackProps } from "@mui/material";
 import ArticleCard from "../../../card/articleCard/ArticleCard";
 
-interface Props {
+interface Props extends StackProps {
   articlesData: ArticleData[];
 }
 
-const ArticlesLayout: FC<Props> = ({ articlesData }) => {
+const ArticlesLayout: FC<Props> = ({ articlesData, ...props }) => {
   return (
-    <Stack gap={"24px"}>
+    <Stack {...props} gap="24px">
       {articlesData.map((data) => (
         <ArticleCard key={data.title} data={data} />
       ))}
